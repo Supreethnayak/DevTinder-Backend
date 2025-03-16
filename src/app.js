45d4bc2cddefa6a74c9dc8b1db56558dddce8321
Supreeth <nayaksupreeth4@gmail.com>
2025-03-16 11:42:14 +0530

    fix -1

--- src/app.js
+++ src/app.js
@@ -7,8 +7,6 @@ const http = require("http");
 
 require("dotenv").config();
 
-require("./utils/cronjob");
-
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -22,25 +20,21 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
-const paymentRouter = require("./routes/payment");
-const initializeSocket = require("./utils/socket");
 const chatRouter = require("./routes/chat");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
-app.use("/", paymentRouter);
 app.use("/", chatRouter);
 
 const server = http.createServer(app);
-initializeSocket(server);
 
 connectDB()
   .then(() => {
     console.log("Database connection established...");
     server.listen(process.env.PORT, () => {
-      console.log("Server is successfully listening on port 7777...");
+      console.log("Server is successfully listening on port", process.env.PORT);
     });
   })
   .catch((err) => {
